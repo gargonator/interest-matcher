4e4e79d5bd03d5130dbec266a77092c12e47603b
BhaskarGarg <bhask.garg@gmail.com>
2018-03-16 18:56:32 -0700

    matching functionality working locally!

--- public/assets/js/sql_test.js
+++ public/assets/js/sql_test.js
@@ -6,15 +6,7 @@ const UserInterest = db.UserInterest;
 const sequelize = db.sequelize;
 const Sequelize = db.Sequelize;
 
-// var results = User.findAll().then(users => console.log(users));
-
-// sequelize.query('SELECT * from Users WHERE id = 2', {type: sequelize.QueryTypes.SELECT}).then(row => {
-// 	console.log(row);
-// })
-
-// results.then((res) => console.log(results));
-
-
+// dummy user for testing purposes
 user = {
     "id": 1,
     "email": "bob@gmail.com",
@@ -24,7 +16,7 @@ user = {
     "picture": "/pic/bob.jpg",
     "phone": "45234523",
     "latlong": JSON.stringify({lat: 47, lng: -122}),
-    "interests": JSON.stringify([1,3]),
+    "interests": JSON.stringify([1,3,6]),
     "createdAt": "2018-03-13T03:43:35.000Z",
     "updatedAt": "2018-03-13T03:43:35.000Z"
 }
@@ -33,136 +25,155 @@ user = {
 // input: user object
 // output: array of matches
 // console.log(Sequelize.literal('CURRENT_TIMESTAMP(3)'));
-getMatches(user);
+// getMatches(user).then(results => console.log(results));
 
-function getMatches(user) {
+function getMatches(user, userid) {
 
-	var getMatches 
-	// array to store matches
-	var matches = [];
+	return new Promise((resolve) => {	
+		// array to store matches
+		var matches = [];
 
-	// store user id
-	var userId = user.id;
+		// store user id
+		var userId = parseInt(userid);
 
-	// store array of interest IDs for user
-	var interestIds = JSON.parse(user.interests);
+		// store array of interest IDs for user
+		var interestIds = JSON.parse(user.interests);
 
-	// for each interest ID for the user, get IDs for other users that have matching interests
-	interestIds.forEach(interestId => {
-		sequelize.query('SELECT UserId FROM UserInterest WHERE InterestId = ? AND UserId <> ?',
-			{replacements: [interestId,userId], type: sequelize.QueryTypes.SELECT}).then(matchedUsers => {
-				// iterate through each matched user
-				matchedUsers.forEach(match => {
-						var matchId = match.UserId;
-						Promise
-							.all([getInterest(interestId),getDistance(userId, matchId)])
-							.then(function(results) {
-								var interest_string = results[0];
-								var distance = results[1];
-								// if match is not already stored
-								if (!isInMatches(match, matches)) {
-									// create match record to store in matches array
-									var matchRecord = {
-											UserId: userId,
-											matched_user: matchId,
-											matched_interests: interest_string,
-											distance: distance,
-											createdAt: Sequelize.literal('CURRENT_TIMESTAMP(3)'),
-											updatedAt: Sequelize.literal('CURRENT_TIMESTAMP(3)')
-										};
-									// push the match record to the matches array
-									matches.push(matchRecord);
-								} else { // augment interests for the match
-									// get index of the match in the matches array
-									var index = matches.findIndex((match) => match.matched_user === matchId);
-									// update interests to include the new matched interest
-									matches[index].matched_interests += ", " +interest_string;
-									matches[index].updatedAt = Sequelize.literal('CURRENT_TIMESTAMP(3)');
-								}
+		// counters for tracking completion of async operations
+		var counter1 = 0; // incremented after each loop
+		var counter2 = 0; // incremented after async operations
+
+		// for each interest ID for the user, get IDs for other users that have matching interests
+		interestIds.forEach(interestId => {
+			sequelize.query('SELECT UserId FROM UserInterest WHERE InterestId = ? AND UserId <> ?',
+				{replacements: [interestId,userId], type: sequelize.QueryTypes.SELECT}).then(matchedUsers => {
+					// add matched users to counter1
+					counter1 = counter1 + matchedUsers.length; 
+					// console.log('Counter1: ',counter1)
+					// iterate through each matched user
+					console.log(`I'm in the first loop!`);
+					console.log(matchedUsers);
+					matchedUsers.forEach(match => {
+							console.log(`I'm in the second loop!`);
+							var matchId = match.UserId;
+							Promise
+								.all([getInterest(interestId),getDistance(userId, matchId)])
+								.then(function(results) {
+									console.log(`The promises resolved!`);
+									var interest_string = results[0];
+									var distance = results[1];
+									// if match is not already stored
+									if (!isInMatches(match, matches)) {
+										// create match record to store in matches array
+										var matchRecord = {
+												UserId: userId,
+												matched_user: matchId,
+												matched_interests: interest_string,
+												distance: distance,
+												// createdAt: Sequelize.literal('CURRENT_TIMESTAMP(3)'),
+												// updatedAt: Sequelize.literal('CURRENT_TIMESTAMP(3)')
+											};
+										// push the match record to the matches array
+										matches.push(matchRecord);
+									} else { // augment interests for the match
+										// get index of the match in the matches array
+										var index = matches.findIndex((match) => match.matched_user === matchId);
+										// update interests to include the new matched interest
+										matches[index].matched_interests += ", " +interest_string;
+										// matches[index].updatedAt = Sequelize.literal('CURRENT_TIMESTAMP(3)');
+									}
+									// update counter2 
+									counter2++;
+									// console.log('Counter 2 :',counter2);
+									// resolve promise if all writes to matches are complete
+									if (counter2 === counter1) {
+										resolve(matches);
+									}
+								});
 							});
 						});
 					});
-				});
+	}); // end promise
 
-	setTimeout(() => console.log(matches),2000);
-};
+	// HELPER FUNCTIONS
+	// --------------------------------------------------------------------------------
 
-// returns true if match is already in matches array, otherwise returns false
-function isInMatches(new_match, matches_array) {
+	// returns true if match is already in matches array, otherwise returns false
+	function isInMatches(new_match, matches_array) {
 
-	var matched = false;
+		var matched = false;
 
-	matches_array.forEach(already_matched => {
-		if (already_matched.matched_user === new_match.UserId) {
-			matched = true;
-		}
-	});
+		matches_array.forEach(already_matched => {
+			if (already_matched.matched_user === new_match.UserId) {
+				matched = true;
+			}
+		});
 
-	return matched;
+		return matched;
 
-}
+	}
 
-// deletes all previous matches in the matches database
-function deletePreviousMatches(user) {
+	// deletes all previous matches in the matches database
+	function deletePreviousMatches(user) {
 
-}
+	}
 
-// returns distance in miles between two users
-function getDistance(user1_id, user2_id) {
-	return new Promise((resolve, reject) => {
-		// get lat long of first user
-		sequelize.query(`SELECT latlong from Users WHERE id = ${user1_id}`, { type: sequelize.QueryTypes.SELECT}).then(results => {
-			var latlng1 = results[0].latlong; // store lat long
-			// get lat long of second user
-			sequelize.query(`SELECT latlong from Users WHERE id = ${user2_id}`, { type: sequelize.QueryTypes.SELECT}).then(results => {
-				var latlng2 = results[0].latlong; // store lat long
-				// return distance between users
-				resolve(distance(latlng1,latlng2))
+	// returns distance in miles between two users
+	function getDistance(user1_id, user2_id) {
+		return new Promise((resolve, reject) => {
+			// get lat long of first user
+			sequelize.query(`SELECT latlong from Users WHERE id = ${user1_id}`, { type: sequelize.QueryTypes.SELECT}).then(results => {
+				var latlng1 = results[0].latlong; // store lat long
+				// get lat long of second user
+				sequelize.query(`SELECT latlong from Users WHERE id = ${user2_id}`, { type: sequelize.QueryTypes.SELECT}).then(results => {
+					var latlng2 = results[0].latlong; // store lat long
+					// return distance between users
+					resolve(distance(latlng1,latlng2))
+				})
 			})
 		})
-	})
+	}
 
-}
+	// returns interest based on interest ID
+	function getInterest(interestId) {
+			return new Promise((resolve, reject) => {
+					resolve(sequelize.query(`SELECT interest from Interests WHERE id = ${interestId}`, { type: sequelize.QueryTypes.SELECT}).then(results => results[0].interest))
+				});
+	};
 
-// returns interest based on interest ID
-function getInterest(interestId) {
-		return new Promise((resolve, reject) => {
-				resolve(sequelize.query(`SELECT interest from Interests WHERE id = ${interestId}`, { type: sequelize.QueryTypes.SELECT}).then(results => results[0].interest))
-			});
-};
+	// 
+	function distance(latlng1, latlng2) {
 
-function distance(latlng1, latlng2) {
+		// parse strings into JSON objects
+		var latlng1 = JSON.parse(latlng1);
+		var latlng2 = JSON.parse(latlng2);
 
-	// parse strings into JSON objects
-	var latlng1 = JSON.parse(latlng1);
-	var latlng2 = JSON.parse(latlng2);
+		// convert latitude and longitude to radians
+		var [lat1, lat2, lng1, lng2] = [latlng1.lat, latlng2.lat, latlng1.lng, latlng2.lng] 
+			.map(elem => elem * Math.PI / 180);
 
-	// convert latitude and longitude to radians
-	var [lat1, lat2, lng1, lng2] = [latlng1.lat, latlng2.lat, latlng1.lng, latlng2.lng] 
-		.map(elem => elem * Math.PI / 180);
+		var deltaLat = lat2 - lat1;
+		var deltaLong = lng2 - lng1;
 
-	var deltaLat = lat2 - lat1;
-	var deltaLong = lng2 - lng1;
+		// constants
+		var R = 6371; // radius of earth in km
+		var kmToMile = 0.621371 // km to mile conversion
 
-	// constants
-	var R = 6371; // radius of earth in km
-	var kmToMile = 0.621371 // km to mile conversion
+		// calculate parameter for the haversine formula
+		var a = Math.sin(deltaLat/2) * Math.sin(deltaLat/2) +
+			Math.cos(lat1) * Math.cos(lat2) * Math.sin(deltaLong/2) * Math.sin(deltaLong/2);
+		
+		// calculate distance based on radius of Earth
+		var d = 2 * R * Math.asin(Math.sqrt(a)) * kmToMile;
 
-	// calculate parameter for the haversine formula
-	var a = Math.sin(deltaLat/2) * Math.sin(deltaLat/2) +
-		Math.cos(lat1) * Math.cos(lat2) * Math.sin(deltaLong/2) * Math.sin(deltaLong/2);
-	
-	// calculate distance based on radius of Earth
-	var d = 2 * R * Math.asin(Math.sqrt(a)) * kmToMile;
+		// return distance
+		return d;
 
-	// return distance
-	return d;
+	}
 
-}
+	// setTimeout(() => console.log(matches),2000);
 
-module.exports = {
-	getMatches: function(user) {
-			getMatches(user)
-		}
-}
+}; // end get Matches function
+
+module.exports = getMatches;
 
---
+++ routes/api-routes.js
@@ -0,0 +1,83 @@
+
+var db = require("../models");
+var computematches = require("../app.js");
+var getMatches = require("../public/assets/js/sql_test.js")
+const User = db.User;
+const Favorite = db.Favorite;
+const Match = db.Match;
+const Interest = db.Interest;
+//get all users or a specific user
+module.exports = function(app){
+    
+    //********** USERS **********************************/
+    
+    // Get route for retrieving a single user
+    app.get("/api/users/:id?", function(req, res) {
+        if(req.params.id){
+            User.findById(req.params.id).then(user => res.json(user));
+        }
+        else{
+            User.findAll().then(user => res.json(user));
+        }
+    });
+    
+    //insert one user
+    app.post('/api/user', function(req, res){
+        // res.render("profile");
+        User.create(req.body).then(newUser => res.json({id: newUser.id}));
+    });
+
+    //update one user
+    app.put('/api/user/:id', function(req, res){
+        User.update(req.body,{
+            where:{
+                id: req.params.id
+            }
+        })
+        .then(results => {
+           return getMatches(req.body, req.params.id);
+        })
+        .then(results => {
+            Match.bulkCreate(results)
+            res.json(results);
+        })
+
+
+        //     function()
+        // {   
+        //     const arrMatches = computematches();
+        //     // console.log("computematches:",arrMatches);
+        //     return Match.bulkCreate(arrMatches);
+        // }).then(matches => res.json({id:matches.id}))
+        
+    });
+
+    //********** Favorites **********************************/
+    
+    //get favorites by user
+    app.get('/api/favorites/:id', function(req,res){
+        Favorite.findAll({
+            where: {
+              UserId: req.params.id
+            }
+          })
+        .then(favorite => res.json(favorite));
+    });
+
+     //********** Matches **********************************/
+    
+    //get matches by user
+    app.get('/api/matches/:id', function(req,res){
+        db.sequelize.query("select * from Users, matches where Users.id = matches.matched_user and matches.UserId = " + req.params.id, { type: db.sequelize.QueryTypes.SELECT})
+        .then(matches => res.json(matches));
+    });
+
+    //********** Interest **********************************/
+    
+    //get all interest
+    app.get('/api/interests/', function(req,res){
+        Interest.findAll()
+        .then(interests => res.json(interests));
+    });
+    
+}
